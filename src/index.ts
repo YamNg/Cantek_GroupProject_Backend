e0d12c299b4fab17280a67bc735a908e07ae2436
Yam Ng <108157094+YamNg@users.noreply.github.com>
2024-01-12 19:12:31 -0500

    add cors policy setting

--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,20 @@ import "dotenv/config";
 import cors from "cors";
 import { connectToMongo } from "./config/mongoose/mongoose.js";
 import routes from "./routes/index.js";
-import cookieParser from 'cookie-parser';
+import cookieParser from "cookie-parser";
 import errorHandlerMiddleware from "./middleware/error-handler.middleware.js";
 
 const app = express();
 
 // Middlewares
+var corsOptions = {
+  origin: process.env.FRONT_END_DOMAIN,
+  credentials: true,
+};
+app.use(cors(corsOptions));
 app.use(express.json());
-app.use(cors());
 app.use(cookieParser());
-app.use('/', routes);
+app.use("/", routes);
 app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT || 3000;
