e2300b75b44c32f8bba5ebe76079eeda8ac9f440
Yam Ng <108157094+YamNg@users.noreply.github.com>
2024-01-14 11:07:22 -0500

    Add reconfirmPassword field and utility to register user endpoint

--- src/controller/validator/user-register.validator.ts
+++ src/controller/validator/user-register.validator.ts
@@ -4,6 +4,7 @@ export const userRegisterValidator = Joi.object({
   email: Joi.string().email().required(),
   username: Joi.string().required(),
   password: Joi.string().required(),
+  reconfirmPassword: Joi.string().required().valid(Joi.ref("password")),
 }).unknown(false);
 
 export const userLoginValidator = Joi.object({
