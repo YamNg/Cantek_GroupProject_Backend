552831118192da418e6039572d903bc69d9b0be9
Yam Ng <108157094+YamNg@users.noreply.github.com>
2024-01-16 20:40:12 -0500

    Development forum (#13)
    * minor changes on command and required dependencies
    
    * [Under Construction] inital setup
    
    * Merge feature-thread branch to development-forum branch - Add endpoint for forum [without user auth] (#1)
    
    * [under construction] add temporary logic for threads
    
    * [under construction] add logic for threads and comments
    
    * Add upvote, downvote function for Comment
    
    * Feature - global response formatting (#2)
    
    * [Under construction] add middleware for global error handling, add generic dto for response
    
    * refractor forum related api, added log4js library, added middleware to handle error formatting
    
    * add back rimraf library for "rm -rf" in windows os
    
    * Change error middleware default message
    
    * bug fix for reply thread comment
    
    * add endpoint to retrieve latest comment from all topics (#4)
    
    * changes according to frontend development
    
    * minor changes
    
    * finish get comment by batch (#5)
    
    * minor changes
    
    * fix totalPage bug
    
    * add pageSize for Thread Detail View usage
    
    * Feature user (#3)
    
    * [under construction] add temporary logic for threads
    
    * [under construction] add logic for threads and comments
    
    * User schema and password+salt generation
    
    * Add upvote, downvote function for Comment
    
    * user login and authentication
    
    * add cookie-parser
    
    * format error code for user, add user cookie auth for routes, add error handler in catch
    
    * fixed merge conflict, changed user error naming, add middleware to routes
    
    * add frontend and backend implentation for cookie refresh, slight change for user controller and user model
    
    * Test middleware, Generic Response user
    
    * add middleware error handler and fix user controller error
    
    * commit user related code
    
    ---------
    
    Co-authored-by: Yam Ng <108157094+YamNg@users.noreply.github.com>
    
    * minor bug fix on comment pagination
    
    * change to post method for batch retrival comments
    
    * Fix middleware refresh token bug, also change cookie format (#6)
    
    * Fix middleware refresh token bug, also change cookie to userid only
    
    * Bug fix cookie
    
    * bug fix on the sorting of batch retrival comments
    
    * Add api for checking cookie
    
    * add cors policy setting
    
    * minor fix
    
    * Add user logout
    
    * adding user cookie middleware for logout function
    
    * Add reconfirmPassword field and utility to register user endpoint
    
    * bug fix
    
    * add  user status (login/logout)
    
    * fix minor bug on verifyUserCookies
    
    * add production setting for cookies
    
    * setting domain to cookie response
    
    * split frontend domain setting into protocol and domain
    
    * trial to resolve cookie problem in prod
    
    * rollback changes
    
    * trial bug fix
    
    ---------
    
    Co-authored-by: alfredthwong <153531926+alfredthwong@users.noreply.github.com>
    Co-authored-by: Jason Tang <39010822+AsonJtsz@users.noreply.github.com>
    Co-authored-by: AsonJtsz <jasontangtsz@gmail.com>

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -111,27 +111,11 @@ export const userLogin = async (
 
     // set secure to true in production so only https can connect
     // secure: false, both http, https can connect
-    if (
-      process.env.ENVIRONMENT &&
-      process.env.ENVIRONMENT === EnvironmentConstants.DEV
-    ) {
-      res.cookie("accessToken", accessToken, { httpOnly: true, secure: false });
-      res.cookie("refreshToken", refreshToken, {
-        httpOnly: true,
-        secure: false,
-      });
-    } else {
-      res.cookie("accessToken", accessToken, {
-        httpOnly: true,
-        sameSite: "none",
-        secure: true,
-      });
-      res.cookie("refreshToken", refreshToken, {
-        httpOnly: true,
-        sameSite: "none",
-        secure: true,
-      });
-    }
+    res.cookie("accessToken", accessToken, { httpOnly: true, secure: false });
+    res.cookie("refreshToken", refreshToken, {
+      httpOnly: true,
+      secure: false,
+    });
 
     res.status(200).send(
       new GenericResponseDto({
